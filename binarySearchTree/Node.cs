425b6bde9ef5221d7f2a9ba528f3764a3542c181
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-02-28 11:34:04 -0600

    Set up Node and BinarySearchTree classes. Input initial logic to get started.

---
+++ binarySearchTree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarySearchTree
+{
+    class Node
+    {
+        int data;
+        public Node link;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
